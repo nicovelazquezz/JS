404f449f64d7b7a3db9bf825ae855508b2e6fe32
nicovelazquezz <112223468+nicovelazquezz@users.noreply.github.com>
2022-11-02 12:00:14 -0300

    Add files via upload

--- js/03-app.js
+++ js/03-app.js
@@ -1,8 +1,7 @@
-let opcion = prompt("Elegí el clima a convertir: \n1- Celsius \n2 - Farenheit \n3 - Kelvin");
+let opcion = prompt("Elegí la temperatura a convertir: \n1- Celsius \n2 - Farenheit \n3 - Kelvin");
 let Celsius = 0;
 let Farenheit = 32;
 let Kelvin = 273.15;
-
 
 switch (opcion) {
     case "1":
@@ -15,29 +14,45 @@ switch (opcion) {
         alert ("Elegiste grados Kelvin.")
         break;
     default:
-        alert ("No ingresaste una opción válida.")     
+        alert ("No ingresaste una opción válida.")  
 }
 
-let aConvertir=parseInt(prompt("Elegí el clima al que querés llegar: \n1- Celsius \n2 - Farenheit \n3 - Kelvin"));
+let aConvertir=parseInt(prompt("Elegí la temperatura a la que querés llegar: \n1- Celsius \n2 - Farenheit \n3 - Kelvin"));
+
+switch (aConvertir) {
+    case 1:
+        alert ("Elegiste grados Celsius.")
+        break;
+    case 2:
+        alert ("Elegiste grados Farenheit.")
+        break;
+    case 3:
+        alert ("Elegiste grados Kelvin.")
+        break;
+    default:
+        alert ("No ingresaste una opción válida.")  
+}
+
 let grados= parseInt(prompt ("Ingresá los grados a convertir"));
 
 if (opcion==1 && aConvertir==2) {
     alert("La temperatura de grados Celsius a Farenheit es de " + (((grados*9)/5)+Farenheit))
-};
-if (opcion==1 && aConvertir==3) {
+}
+else if (opcion==1 && aConvertir==3) {
     alert("La temperatura en grados Celsius a Kelvin es de " + (grados+Kelvin))
-};
-if (opcion==2 && aConvertir==1) {
+}
+else if (opcion==2 && aConvertir==1) {
     alert("La temperatura en grados Farenheit a Celsius es de " +(((grados-32)*5)/9))    
-};
-if (opcion==2 && aConvertir==3) {
+}
+else if (opcion==2 && aConvertir==3) {
     alert("La temperatura en grados Kelvin a Farenheit es de " + (((grados-32)*(5/9)+273.15)))
-};
-if (opcion==3 && aConvertir==1) {
+}
+else if (opcion==3 && aConvertir==1) {
     alert("La temperatura en grados Kelvin es de " + (grados-Kelvin))
-};
-if (opcion==3 && aConvertir==2) {
+}
+else if (opcion==3 && aConvertir==2) {
     alert("La temperatura en grados Kelvin es de " + ((((grados-Kelvin)*9)/5)+32))
 } 
+else {alert("Error, no elegiste ni 1, 2 o 3")};
 
 
---
+++ js/main.js
@@ -0,0 +1,53 @@
+const productos =  [
+    {nombre:'Samsung S22', precio:1000, origen: "celular"},
+    {nombre:'iPhone 13 Pro Max', precio:1199, origen: "celular"},
+    {nombre:'iPhone X', precio:600, origen: "celular"},
+    {nombre:'Notebook Gamer AORUS', precio:3300, origen: "notebook"},
+    {nombre:'Notebook Gamer Acer NITRO 5', precio:1990, origen: "notebook"},
+    {nombre:'Notebook Gamer Asus TUF FX516', precio:2500, origen: "notebook"}
+]
+
+let categoriaProducto=prompt('Elegí el tipo del producto que querés comprar: \n1 - Celular \n2 - Notebook');
+
+
+switch (categoriaProducto) {
+    case "1":
+        categoria = productos.filter(producto => producto.origen === "celular");
+        categoria.forEach (productos => console.log(`El producto ${productos.nombre} tiene un precio de $${productos.precio}`));
+        itemSelect=prompt(`Elegí un producto para calcular el IVA: \n1 - ${categoria[0].nombre} - $${categoria[0].precio} \n2 - ${categoria[1].nombre} - $${categoria[1].precio} \n3 - ${categoria[2].nombre} - $${categoria[2].precio}`);        
+        switch (itemSelect) {
+            case "1":
+                alert("El precio con iva es $" + categoria[0].precio*1.21);                
+                break;
+            case "2":
+                alert("El precio con iva es $" + categoria[1].precio*1.21);
+                break;
+            case "3":
+                alert("El precio con iva es $" + categoria[2].precio*1.21);
+                break;        
+            default:
+                alert ("No ingresaste una opción válida."); 
+            };
+    break;
+    case "2":        
+        categoria = productos.filter(producto => producto.origen === "notebook");
+        categoria.forEach (productos => console.log(`El producto ${productos.nombre} tiene un precio de $${productos.precio}`));        
+        itemSelect=prompt(`Elegí un producto para calcular el IVA: \n1 - ${categoria[0].nombre} \n2 - ${categoria[1].nombre} \n3 - ${categoria[2].nombre}`);
+        switch (itemSelect) {
+            case "1":
+                alert("El precio con iva es $" + categoria[0].precio*1.21);                
+                break;
+            case "2":
+                alert("El precio con iva es $" + categoria[1].precio*1.21);
+                break;
+            case "3":
+                alert("El precio con iva es $" + categoria[2].precio*1.21);
+                break;    
+            default:
+                alert ("No ingresaste una opción válida.");     
+            };
+break;
+default:
+alert ("No ingresaste una opción válida."); 
+}
+
